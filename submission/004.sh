1c5da204e9d1dd68b07c3dac21882c3dab7db939
Ângelo K <misterangelo21@hotmail.com>
2025-02-02 16:00:35 -0300

    Update para a questão 4

--- submission/004.sh
+++ submission/004.sh
@@ -2,4 +2,5 @@
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 trpubkey="tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/100)"
 descriptor=$(bitcoin-cli getdescriptorinfo "$trpubkey" | jq ."descriptor")
-eval "bitcoin-cli deriveaddresses $descriptor" | jq .[0]
+addr=$(eval "bitcoin-cli deriveaddresses $descriptor" | jq .[0])
+printf $addr
